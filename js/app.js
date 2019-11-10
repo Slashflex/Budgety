f218fcbd7c84322794cbd20e01fb98ecd96f8658
slashflex <pro.davidsaoud@gmail.com>
2019-11-10 14:54:31 +0100

    Event Listeners on click and entre keypress

--- js/app.js
+++ js/app.js
@@ -1,20 +1,13 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
 
-    var x = 23;
-
-    var add = function(a) {
-        return x + a; 
-    }
-
-    return {
-        publicTest: function(b) {
-            return add(b);
-        }
-    }
+    // Some code
 
 })(); // budgetController.publicTest(5) = 23 + 5 = 28
+ 
 
 
+// UI CONTROLLER
 var UIController = (function() {
 
     // Some code
@@ -22,14 +15,34 @@ var UIController = (function() {
 })();
 
 
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UIctrl) {
 
-    var z = budgetCtrl.publicTest(5);
+    var ctrlAddItem = function() {
 
-    return {
-        anotherPublic: function() {
-            console.log(z);
-        }
+        // 1. Get user input data
+
+
+        // 2. Add the item to the budget controller
+
+
+        // 3. Add the item to the UI
+
+        
+        // 4. Calculate the budget
+
+
+        // 5. Display the budget on the UI
     }
 
-})(budgetController, UIController); // controller.anotherPublic() = 28
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+
+        if (event.keyCode === 13 || event.which === 13) {
+
+        }
+    });
+
+})(budgetController, UIController); 
