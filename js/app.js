f7c3282e75514f18114e9ae8dbeb9cf6b8ae6b31
slashflex <pro.davidsaoud@gmail.com>
2019-11-10 14:56:46 +0100

    callback fuunction fix

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ var controller = (function(budgetCtrl, UIctrl) {
     document.addEventListener('keypress', function(event) {
 
         if (event.keyCode === 13 || event.which === 13) {
-
+            ctrlAddItem();
         }
     });
 
